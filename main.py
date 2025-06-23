a8f7a590255fb59fbb2848d4873ced2a64267516
DiegoPortillaC <diegoportillacosta.g2@gmail.com>
2025-06-23 14:09:50 +0200

    Creacion de funciones 2-3-4
    Creación de :
    -Insert factura
    -mostrar todos los usuarios
    -Listado de facturas por cliente
    
    Añadida la bbdd al gitignore

--- main.py
+++ main.py
@@ -11,29 +11,30 @@ while True:
     try:
         main_answer = main_menu()
         if main_answer == 1:
-            #insert usuario
             insert_user(conn, cursor)
 
-        if main_answer == 2:
-            #select usuario por id
-            selec_user(conn, cursor)
+        elif main_answer == 2:
+            select_user(conn, cursor)
 
-        if main_answer == 3:
-            #insert factura
-            print("Hasta la proxima")    
-        if main_answer == 4:
-            #select * usuarios
-            print("Hasta la proxima")    
-        if main_answer == 5:
-            #select * from facturas where user_id = X
-            print("Hasta la proxima")    
-        if main_answer == 6:
+        elif main_answer == 3:
+            insert_factura(conn, cursor)
+
+        elif main_answer == 4:
+            show_all_users(cursor)
+        
+        elif main_answer == 5:
+            user_bill_list(cursor)
+          
+        elif main_answer == 6:
             #select * from facturas group by usuario + sumatorios de cantidades
-            print("Hasta la proxima")    
-        if main_answer == 7:
+            print("Hasta la proxima")  
+          
+        elif main_answer == 7:
             print("Hasta la proxima")    
             break
+
         else:
-            print("\nElegiste una opcion no presnte")
+            print("\nElegiste una opcion no presente")
+
     except ValueError:
         print("\nIntrodujiste un caracter no deseado")
